0b57f731887ace66e21dcfd9706a2b653787d183
simoneaucello <simoneaucello@gmail.com>
2024-05-09 14:51:43 +0200

    [add] header refactoring

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<header>
+    <div class="sa_container menu">
+        <ul class="d-flex justify-content-around">
+            <li><a href="#">Uomo</a></li>
+            <li><a href="#">Donna</a></li>
+            <li><a href="#">Bambino</a></li>
+        </ul>
+
+        <div class="logo">
+            <img src="/img/boolean-logo.png" alt="logo">
+        </div>
+
+        <div>
+            <a href="#"><i class="fa-regular fa-user"></i></a>
+            <a href="#"><i class="fa-regular fa-heart"></i></a>
+            <a href="#"><i class="fa-solid fa-cart-shopping"></i> </a>
+        </div>
+    </div>
+</header>
