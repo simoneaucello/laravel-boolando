d0f2bf4e2c1e8096739872d45252ceaae54cbdd0
simoneaucello <simoneaucello@gmail.com>
2024-05-09 15:40:35 +0200

    [add] cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,11 @@
 
         @foreach ($products as $product)
             <div class="sa_card">
-                <div class="card-top">
-                    <img src="/img/{{ $product['frontImage'] }}" alt="">
+                <div class="sa_card_top">
+                    <img class="front-img" src="/img/{{ $product['frontImage'] }}" alt="">
+                    <img class="hover-img" src="/img/{{ $product['backImage'] }}" alt="">
+
+
 
                 </div>
             </div>
